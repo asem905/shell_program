80ed9e34223554404aaacfa3ba279dd773584a5d
asem905 <123882184+asem905@users.noreply.github.com>
2024-08-07 16:58:39 +0300

    Update commands.h

--- commands.h
+++ commands.h
@@ -19,15 +19,25 @@ comment:contains all functions declarartions used in shell.c
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/sysinfo.h>
+#include <time.h>
+
 /*----------------macros declarations-----------------*/
 #define STDIN 0
 #define STDOUT 1
 #define STDERR 2
 #define BUF_SIZE 4096
-#define MAX_TOKENS 100
+#define TOKEN_LENGTH 30
 #define MAX_TOKEN_LENGTH 100
 #define MAX_PATH_LENGTH 1024
 #define MAX_ARGS 100
+#define BUF_SIZE_MEM 256
+#define MAX_LINE 1024
+#define MAX_TOKENS 50
+#define MAX_CMD_LENGTH 1024
+#define MAX_ARG_LENGTH 100
+#define MAX_ARGS 100
+
 /*-------------------extern variables------------------*/
 extern char **environ;
 /*-----------section contains functions declarations used inshell.c file------------*/
@@ -40,4 +50,7 @@ char is_internal(const char *command,const char **commands_given);
 int is_external(char *command);
 void execute_command(char command[][50],int count);
 void echo_command(int arg_count, char args[][50]);
+void free_command();
+void get_uptime_with_time();
+void pipe_commands(char *cmd1, char *cmd2);
 #endif
